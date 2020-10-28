deb32b8e1539d721fc121a64201bc4ed1732fe7a
Will Weiss <will.weiss1230@gmail.com>
2020-10-27 23:57:23 -0400

    env

--- env.js
+++ env.js
@@ -3,8 +3,3 @@ if (!process.env.NODE_ENV) {
 }
 
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
-
-if (process.env.NODE_ENV === 'prod') {
-  pg.defaults.ssl = true
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
-}
--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,10 @@
+require('./env')
 const pg = require('pg')
-
-if (!process.env.NODE_ENV) {
-  throw new Error('Must set NODE_ENV')
-}
 
 if (process.env.NODE_ENV === 'prod') {
   pg.defaults.ssl = true
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 }
-
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
 const connection = process.env.DATABASE_URL || {
   host: process.env.DB_HOST,
