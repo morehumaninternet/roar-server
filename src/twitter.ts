c14d8027547863d8b88c6f91c621e9acff6655ca
Shachar Langer <shachar.langer@gmail.com>
2020-11-02 17:37:37 +0100

    Post status with screenshots

--- src/twitter.ts
+++ src/twitter.ts
@@ -1,6 +1,19 @@
+import { promisify } from 'util'
+import * as fs from 'fs'
 import * as Twit from 'twit'
+import { File } from 'formidable'
 
-export const tweetStatus = async (status: string, access_token: string, access_token_secret: string) => {
+const readFile = promisify(fs.readFile)
+
+type TweetStatusArgs = {
+    status: string,
+    screenshots: ReadonlyArray<File>,
+    access_token: string,
+    access_token_secret: string
+}
+
+export const tweetStatus = async ({ status, screenshots, access_token, access_token_secret }: TweetStatusArgs) => {
+
     const T = new Twit({
         consumer_key: process.env.TWITTER_API_KEY!,
         consumer_secret: process.env.TWITTER_KEY_SECRET!,
@@ -9,7 +22,18 @@ export const tweetStatus = async (status: string, access_token: string, access_t
         timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
     })
 
-    const tweet = { status }
+    const media_ids = await Promise.all(
+        screenshots.map(async screenshot => {
+            const options = {
+                encoding: 'base64'
+            }
+            const base64Screenshot = await readFile(screenshot.path, options)
+            const { data } = await T.post('media/upload', { media_data: base64Screenshot })
+            return (data as any).media_id_string
+        })
+    )
+
+    const tweet = { status, media_ids }
     const { data, resp } = await T.post('statuses/update', tweet)
     return { data, resp }
 }
