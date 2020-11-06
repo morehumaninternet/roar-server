2c53677d2f692f2bd28a3c7cfb4e77e69ffd9fcf
Will Weiss <will.weiss1230@gmail.com>
2020-11-06 13:35:04 -0500

    rm console.log

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,7 @@ export const trackRequests: IMiddleware = (
   async ({ response }: IRouterContext, next) => {
 
     /* tslint:disable:no-expression-statement */
-    try {
-      await next()
-      console.log(response)
-    } catch (err) {
+    try { await next() } catch (err) {
       const status = err.status || 500
       Object.assign(response, { status, body: err.message })
       if (status >= 500) console.error(err)
