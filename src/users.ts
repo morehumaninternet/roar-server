45021c0c0e4abc649aa85f3c216db8e9bb275806
Will Weiss <will.weiss1230@gmail.com>
2020-11-09 13:36:57 -0500

    include photoUrl for user on twitter-auth-success

--- src/users.ts
+++ src/users.ts
@@ -1,6 +1,7 @@
 
 import { Profile as TwitterProfile } from 'passport-twitter'
 import db from './db'
+const upsert = require('knex-upsert')
 
 export function fetchUser(id: number): Promise<Maybe<User>> {
   return db<User>('users').where('id', id).first()
@@ -10,14 +11,25 @@ export function fetchUserByTwitterId(twitterId: string): Promise<Maybe<User>> {
   return db<User>('users').where('twitter_id', twitterId).first()
 }
 
-export async function createUserFromTwitterProfile(profile: TwitterProfile): Promise<User> {
-  const [user] = await db<User>('users').insert({
-    twitter_id: profile.id,
-    twitter_handle: profile.username,
-    display_name: profile.displayName,
-    photo: profile.photos && profile.photos[0].value,
-    email: profile.emails && profile.emails[0].value
-  }).returning('*')
+function photo(profile: TwitterProfile): undefined | string {
+  const firstPhoto = profile.photos?.[0]?.value
+  // Don't use the default profile image from twitter
+  if (firstPhoto && firstPhoto !== 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png') {
+    return firstPhoto
+  }
+}
 
-  return user
+export function upsertWithTwitterProfile(profile: TwitterProfile): Promise<User> {
+  return upsert({
+    db,
+    table: 'users',
+    object: {
+      twitter_id: profile.id,
+      twitter_handle: profile.username,
+      display_name: profile.displayName,
+      photo: photo(profile),
+      email: profile.emails && profile.emails[0].value
+    },
+    key: 'twitter_id',
+  })
 }
