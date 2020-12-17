b6a08badfb58e782eaa2dae3479a963ae8da7631
Shachar Langer <shachar.langer@gmail.com>
2020-12-17 18:39:28 +0200

    Fix Twitter auth

--- src/handlers.ts
+++ src/handlers.ts
@@ -77,13 +77,13 @@ export async function getWebsite(ctx: IRouterContext): Promise<any> {
 // Redirect the user to Twitter for authentication. When complete, Twitter
 // will redirect the user back to the application at
 // /auth/twitter/callback
-export const authTwitter = () => passport.authenticate('twitter')
+export const authTwitter = passport.authenticate('twitter')
 
 // Twitter will redirect the user to this URL after approval. Finish the
 // authentication process by attempting to obtain an access token. If
 // access was granted, the user will be logged in. Otherwise,
 // authentication has failed.
-export const authTwitterCallback = () => passport.authenticate('twitter', {
+export const authTwitterCallback = passport.authenticate('twitter', {
   successRedirect: '/v1/auth/twitter/success',
   failureRedirect: '/v1/auth/twitter/failure'
 })
