d231677c5b931e1ca6aeea56fdfcf5bbd9ab8dd4
Will Weiss <will.weiss1230@gmail.com>
2020-12-17 12:12:56 -0500

    welcome and html auth success/failure pages

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,9 @@ import { identity } from 'lodash'
 import * as Router from 'koa-router'
 import * as send from 'koa-send'
 import { readdirSync } from 'fs'
+import { parse } from 'path'
 import * as handlers from './handlers'
+
 
 
 export function createRouter(withRouter: (router: Router) => Router = identity): Router {
@@ -11,8 +13,6 @@ export function createRouter(withRouter: (router: Router) => Router = identity):
     .get('/website', handlers.getWebsite)
     .get('/auth/twitter', handlers.authTwitter)
     .get('/auth/twitter/callback', handlers.authTwitterCallback)
-    .get('/auth/twitter/success', handlers.authTwitterSuccess)
-    .get('/auth/twitter/failure', handlers.authTwitterFailure)
     .get('/me', handlers.getMe)
     .post('/feedback', handlers.postFeedback)
     .post('/logout', handlers.logout)
@@ -24,8 +24,11 @@ export function createRouter(withRouter: (router: Router) => Router = identity):
     .use('/v1', v1Router.routes(), v1Router.allowedMethods())
 
   // tslint:disable-next-line:no-expression-statement
-  readdirSync(process.cwd() + '/public').forEach(fileName =>
-    router.get('/' + fileName, ctx => send(ctx, `/public/${fileName}`)))
+  readdirSync(process.cwd() + '/public').forEach(fileName => {
+    const file = parse(fileName)
+    const route = file.ext === '.html' ? file.name : fileName
+    return router.get('/' + route, ctx => send(ctx, `/public/${fileName}`))
+  })
 
   return router
 }
