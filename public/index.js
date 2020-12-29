506cfde1ef5d2b3019cb3fa1dd40897fc92e9731
Shachar Langer <shachar.langer@gmail.com>
2020-12-29 15:24:41 +0200

    Landing page - style changes for the first section

--- public/index.js
+++ public/index.js
@@ -1,7 +1,28 @@
+/*
+  Open the video modal when clicking on "Watch Video" or the gif.
+  Close the video modal when clicking outside the video or pressing on the Escape key.
+*/
+
 const toggleModal = () => {
   document.querySelector(".modal").classList.toggle("modal--hidden")
 }
 
-document.querySelector(".btn__watch").addEventListener("click", toggleModal)
-document.querySelector(".hero-image").addEventListener("click", toggleModal)
+document.querySelector(".btn--watch").addEventListener("click", toggleModal)
+document.querySelector(".hero__gif").addEventListener("click", toggleModal)
 document.querySelector(".modal").addEventListener("click", toggleModal)
+
+document.onkeydown = (event) => {
+  event = event || window.event
+  let isEscape = false
+  if ("key" in event) {
+    // For IE/EDGE the key is "Esc"
+    isEscape = event.key === "Escape" || event.key === "Esc"
+  } else {
+    // Support for old browsers
+    isEscape = event.keyCode === 27
+  }
+  const modalClassNames = document.querySelector(".modal").classList
+  if (isEscape && !modalClassNames.contains("modal--hidden")) {
+    toggleModal()
+  }
+}
