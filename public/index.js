457c769b984fb6f299f3509352d2aa600daf6e09
Hank Liu <hanke.liu@gmail.com>
2020-12-30 14:11:29 -0500

    merge faq.js into index.js file

--- public/index.js
+++ public/index.js
@@ -26,3 +26,20 @@ document.onkeydown = (event) => {
     toggleModal()
   }
 }
+
+/*
+  Toggle FAQ item content when clicking on FAQ headline.
+*/
+const acc = document.getElementsByClassName("accordion");
+
+Array.prototype.forEach.call(acc, (el) => {
+  el.addEventListener("click", () => {
+    el.classList.toggle("active");
+    let panel = el.nextElementSibling; 
+    if (panel.style.display === 'block') {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  })
+})
