d6018ae815a739c283497b4fd3e69652c9ec69df
Will Weiss <will.weiss1230@gmail.com>
2021-01-03 16:22:52 -0500

    attempt github tests

--- gh-test-setup.sh
+++ gh-test-setup.sh
@@ -1,4 +1,21 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-echo "TODO!"
+if [ -z "$GITHUB_REF" ]; then
+  echo "Only github actions should run this script"
+  exit 1
+fi
+
+cat << EOF > .env.test
+DB_HOST=postgres
+DB_PASS=password
+DB_USER=roar
+DB_NAME=roar_test
+
+TWITTER_API_KEY=aaaaaaaaaaaaaaaaaaaaaaaaa
+TWITTER_KEY_SECRET=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+TWITTER_ACCESS_TOKEN=aaaaaaaaaaaaaaaaaaa-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+TWITTER_TOKEN_SECRET=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+EOF
+
+NODE_ENV=test npm run knex migrate:latest
